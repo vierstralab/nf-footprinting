bb3bc19867116ca91063995318537412b939b782
Sergey Abramov <aswq22013@gmail.com>
2026-07-06 22:58:28 +0300

    fix

--- bin/extract_fp_data.py
+++ bin/extract_fp_data.py
@@ -46,7 +46,7 @@ if __name__ == "__main__":
         sample_data
     )
 
-    to_npz(data=data, path=sys.argv[4])
+    data.to_npz(data=data.differential, path=sys.argv[4])
     # dhs_region = GenomicInterval("chr15", 74995300, 74995600)
 
 
